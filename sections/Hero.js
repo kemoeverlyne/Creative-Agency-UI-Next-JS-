d3193a9bb0cc301f7c525baa1aac0b82087121b3
kemoeverlyne <kemoeverlyne@gmail.com>
2023-08-02 14:55:16 +0300

    reduce video quality

--- sections/Hero.js
+++ sections/Hero.js
@@ -6,7 +6,7 @@ import Testimonial from "@/components/Testimonial"
 import { Title, TitleLogo, TitleSm } from "@/components/common/Title"
 import { BlogCard, Brand } from "@/components/router"
 import React from "react"
-import video from "@/assets/video1.mp4";
+import video from "@/assets/video3.mp4";
 
 const Hero = () => {
   return (
